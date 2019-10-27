5dd1c2e5fc3615b621b3d6e9a4e1366ad88d85fe
Yukuo Cen <cenyk1230@qq.com>
2019-10-27 15:37:46 +0800

    Personal Info

--- frontend/src/components/PersonalInfo.vue
+++ frontend/src/components/PersonalInfo.vue
@@ -7,231 +7,399 @@
     ref="form"
     v-observe-visibility="visibilityChanged"
   >
-    <el-form-item label="姓名" prop="name">
-      <el-input
-        v-model="perinfo.name"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        disabled
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="班级"
-      prop="class_name"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.class_name"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="性别"
-      prop="gender"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.gender"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+    <div v-if="!is_privileged">
+      <el-form-item label="姓名" prop="name">
+        <el-input
+          v-model="perinfo.name"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          disabled
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="班级"
+        prop="class_name"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in gender_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="系所"
-      prop="department"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.department"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-input
+          v-model="perinfo.class_name"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="性别"
+        prop="gender"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in department_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="类别"
-      prop="student_type"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.student_type"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-select
+          v-model="perinfo.gender"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in gender_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="系所"
+        prop="department"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in type_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="年级"
-      prop="grade"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.grade"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-select
+          v-model="perinfo.department"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in department_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="类别"
+        prop="student_type"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in grade_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="学籍状态"
-      prop="student_status"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.student_status"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-select
+          v-model="perinfo.student_type"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in type_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="年级"
+        prop="grade"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in status_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="政治面貌"
-      prop="political_status"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.political_status"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-select
+          v-model="perinfo.grade"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in grade_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="学籍状态"
+        prop="student_status"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in political_status_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      label="民族"
-      prop="ethnic_group"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.ethnic_group"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="导师"
-      prop="instructor"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.instructor"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="邮箱"
-      prop="email"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.email"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="手机"
-      prop="mobile"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.mobile"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="地址"
-      prop="address"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.address"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="邮编"
-      prop="post_code"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-input
-        v-model="perinfo.post_code"
-        placeholder="请输入内容"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
-      ></el-input>
-    </el-form-item>
-    <el-form-item
-      label="是否已开题"
-      prop="is_project_started"
-      :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
-    >
-      <el-select
-        v-model="perinfo.is_project_started"
-        placeholder="请选择"
-        v-bind:style="{ width: elemWidth + 'vw' }"
-        :disabled="disabled"
+        <el-select
+          v-model="perinfo.student_status"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in status_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="政治面貌"
+        prop="political_status"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
       >
-        <el-option
-          v-for="item in is_projected_started_list"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        ></el-option>
-      </el-select>
-    </el-form-item>
+        <el-select
+          v-model="perinfo.political_status"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in political_status_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        label="民族"
+        prop="ethnic_group"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.ethnic_group"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="导师"
+        prop="instructor"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.instructor"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="邮箱"
+        prop="email"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.email"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="手机"
+        prop="mobile"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.mobile"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="地址"
+        prop="address"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.address"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="邮编"
+        prop="post_code"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-input
+          v-model="perinfo.post_code"
+          placeholder="请输入内容"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        ></el-input>
+      </el-form-item>
+      <el-form-item
+        label="是否已开题"
+        prop="is_project_started"
+        :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+      >
+        <el-select
+          v-model="perinfo.is_project_started"
+          placeholder="请选择"
+          v-bind:style="{ width: elemWidth + 'vw' }"
+          :disabled="disabled"
+        >
+          <el-option
+            v-for="item in is_projected_started_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+    </div>
+
+
+    <div v-if="is_privileged">
+      <el-col :span="8">
+        <el-form-item label="姓名" prop="name">
+          <el-input
+            v-model="perinfo.name"
+            placeholder="请输入内容"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            disabled
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="班级"
+          prop="class_name"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-input
+            v-model="perinfo.class_name"
+            placeholder="请输入内容"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="性别"
+          prop="gender"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.gender"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in gender_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="系所"
+          prop="department"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.department"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in department_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="类别"
+          prop="student_type"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.student_type"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in type_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="8">
+        <el-form-item
+          label="年级"
+          prop="grade"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.grade"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in grade_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="政治面貌"
+          prop="political_status"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.political_status"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in political_status_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item
+          label="民族"
+          prop="ethnic_group"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-input
+            v-model="perinfo.ethnic_group"
+            placeholder="请输入内容"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="导师"
+          prop="instructor"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-input
+            v-model="perinfo.instructor"
+            placeholder="请输入内容"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="是否已开题"
+          prop="is_project_started"
+          :rules="[{ required: true, message: '字段不能为空', trigger: ['change','blur'] }]"
+        >
+          <el-select
+            v-model="perinfo.is_project_started"
+            placeholder="请选择"
+            v-bind:style="{ width: elemWidth + 'vw' }"
+            :disabled="disabled"
+          >
+            <el-option
+              v-for="item in is_projected_started_list"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+    </div>
   </el-form>
 </template>
 
@@ -301,6 +469,7 @@ export default {
   },
   data() {
     return {
+      is_privileged: window.sessionStorage.user_type === "1" || window.sessionStorage.user_type === "2",
       perinfo: {
         name: "",
         class_name: "",
